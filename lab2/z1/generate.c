fdc1d9c4445762990e664b7f23c1eb2b059421a9
ixf_lxss <korpus321@gmail.com>
2018-03-17 16:34:59 +0100

    Changes + t.sh

--- lab2/z1/generate.c
+++ lab2/z1/generate.c
@@ -6,7 +6,7 @@ int generate(const char* source, int records, int record_length){
 	      
     char* buf = malloc(record_length);
     FILE* f = fopen(source, "w");
-    FILE* randomIn = fopen("/dev/urandom", "r");
+    FILE* random = fopen("/dev/urandom", "r");
 
     if(random == NULL){
       printf("Error at open(\"dev/urandom\")\n");
---
+++ lab2/z1/t.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+exe() { echo "\$ $@" ; "$@" ; }
+
+if [ "$1" == "generate" ]; then
+exe ./main generate kappa.txt 10 4
+elif [ "$1" == "sort" ]; then
+exe ./main sort kappa.txt 10 4 $2
+elif [ "$1" == "copy" ]; then
+exe ./main copy kappa.txt kappa_copy.txt 10 4 $2
+elif [ "$1" == "view" ]; then
+else
+echo "Wrong arg 1"
+fi
