1af7f1703f98f82fefd1c4f2339c89f587225d2a
bortek <korpus321@gmail.com>
2018-04-14 14:19:53 +0200

    >> i > dla cw05 zad1

--- SofinskiBartosz/cw05/zad1/main.c
+++ SofinskiBartosz/cw05/zad1/main.c
@@ -5,18 +5,13 @@
 #include <unistd.h>
 #include <string.h>
 
+#define FAIL(text, ...) { fprintf(stderr, text, ##__VA_ARGS__); exit(1); }
+
 int main(int argc, char** argv){
 
-  if(argc < 2){
-    printf("Not enough arguments\n");
-    return -1;
-  }
-
+  if(argc < 2) FAIL("not enough arguments!\n");
   FILE* f = fopen(argv[1], "r");
-  if( f == NULL ){
-    printf("File does not exist\n");
-    return -2;
-  }
+  if( !f ) FAIL("file does not exist!\n");
 
   char* line = NULL;
   size_t n = 0;
@@ -26,19 +21,40 @@ int main(int argc, char** argv){
 
   while( getline(&line, &n, f) != -1 ){ 
     line[strlen(line)-1] = 0; // usuwanie \n
-
     int p = 0, a = 0; // proces, argument
+
     args[p][a] = strtok(line, " ");
 
     do {
       a += 1;
       args[p][a] = strtok(NULL, " ");
-      if( args[p][a] != NULL && args[p][a][0] == '|' ){
-	args[p][a] = NULL;
-	p += 1;
-	a = 0;
-	args[p][a] = strtok(NULL, " ");
-      }
+      if( args[p][a] != NULL){
+	if( args[p][a][0] == '|' ){
+	  args[p][a] = NULL;
+	  p += 1;
+	  a = 0;
+	  args[p][a] = strtok(NULL, " ");
+	} else if ( args[p][a][0] == '>' ){
+	  p += 1;
+	  args[p][0] = "dd";
+	  args[p][1] = (char*)malloc(80);
+	  strcpy(args[p][1], "of=");
+	  char* tmp = strtok(NULL," ");
+	  strcat(args[p][1], tmp);
+	  args[p][2] = "status=none";
+
+	  args[p][3] = NULL;
+	  if( args[p-1][a][1] == '>' ){
+	    args[p][3] = "conv=notrunc";
+	    args[p][4] = "oflag=append";
+	    args[p][5] = NULL;
+	  }
+	  args[p-1][a] = NULL;
+	  p += 1;
+	  a = 0;
+	  args[p][a] = strtok(NULL, " ");
+	}
+      } 
     } while( args[p][a] != NULL );
 
     int pipes[2][2];
@@ -58,14 +74,8 @@ int main(int argc, char** argv){
       }
       if( i < p && pipe(pipes[i%2]) < 0 ) perror("failed to pipe\n");
 
-      if(i > 0) wait(&status);
-      int code;
-      if( WIFEXITED(status) && (code = WEXITSTATUS(status)) != 0 ){
-	printf("Error: %s has returned %d\n", args[i-1][0], code);
-	return -1;
-      }
-
       if ( fork() == 0 ){ // child
+
 	if( i > 0 ){
 	  close(pipes[(i+1)%2][1]);
 	  dup2(pipes[(i+1)%2][0], STDIN_FILENO);
@@ -76,12 +86,19 @@ int main(int argc, char** argv){
 	}
 
 	execvp(args[i][0], args[i]);
-	perror("exec error!\n");
+	perror("exec error");
 	exit(1);
-      }
+      } else if(i > 0){ // dla kazdego poza pierwszym
+	  wait(&status);
+	  int code;
+	  if( WIFEXITED(status) && (code = WEXITSTATUS(status)) != 0 ){
+	    printf("Error: %s has returned %d\n", args[i-1][0], code);
+	    return -1;
+	  }
+	}
     } 
-    close(pipes[p%2][0]);
-    close(pipes[p%2][1]);
+    for(int x = 0; x < 4; x++)
+	close(pipes[x/2][x%2]);
 
     // oczekuj na zamkniecie ostatniego procesu
     wait(&status);
@@ -91,9 +108,7 @@ int main(int argc, char** argv){
       return -1;
     }
   }
-
   free(line);
   fclose(f);
   return 0;
 }
-
