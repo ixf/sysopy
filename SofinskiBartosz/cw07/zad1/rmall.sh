3a5933870a20bdda6b1c06902322ffd53e38aacd
Bartosz Sofiński <bsofinsk@d17-jagular.ipa.iisg.agh.edu.pl>
2018-05-14 19:18:51 +0200

    done po raz trzeci

--- SofinskiBartosz/cw07/zad1/rmall.sh
+++ SofinskiBartosz/cw07/zad1/rmall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ipcs -m | grep 644 | awk '{print $2}' | while read in; do ipcrm shm $in; done
-ipcs -s | grep 644 | awk '{print $2}' | while read in; do ipcrm sem $in; done
+ipcs -m | grep bsofinsk | awk '{print $2}' | while read in; do ipcrm shm $in; done
+ipcs -s | grep bsofinsk | awk '{print $2}' | while read in; do ipcrm sem $in; done
