a7af541a3e6289ea2966ff70d4ff273a73e90993
Bartosz Sofiński <bartoszsofinski@gmail.com>
2019-02-10 23:55:03 +0100

    usunięte stare zadania

--- sysopy_stare/lab1/zd1 (1)/block_array.h
+++
@@ -1,9 +0,0 @@
-
-char** create_block_array_dynamic(int elems, int block_size);
-void create_block_dynamic(char** block_array, int elem, int block_size);
-void create_block_static(int elem);
-void delete_block_dynamic(char** block_array, int elem, int block_size);
-void delete_block_static(int elem);
-void delete_block_array_dynamic(char** block_array, int elems);
-int find_nearest_dynamic(char** block_array, int elem, int elems, int block_size);
-int find_nearest_static(int elem);
